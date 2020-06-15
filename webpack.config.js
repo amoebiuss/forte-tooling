f12d407f42898a83997f937658fb9b4e5daf957e
Alena Vasilchenko <vasilchenko.alen@yandex.ru>
2020-06-15 20:30:15 +0300

    output config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    log: './src/js/logger.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Tooling hometask, webpack project from scratch'
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js',
   },
   mode: 'none',
   module: {
